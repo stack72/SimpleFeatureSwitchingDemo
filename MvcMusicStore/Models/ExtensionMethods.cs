0865f8add097a5fc5400090b1fbe8640c31d2b5e
Paul Stack <paulstack@hotmail.com>
2011-08-03 22:10:43 +0100

    addition of work to switch on the home page with a custom html helper

---
+++ MvcMusicStore/Models/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using MvcMusicStore.Models.Configuration;
+
+namespace MvcMusicStore.Models
+{
+    public static class ExtensionMethods
+    {
+        public static bool FeatureSwitch(this HtmlHelper helper, string featureName)
+        {
+            var featureManager = new FeatureManager();
+
+            return featureManager.GetSwitchSetting<bool>(featureName);
+        }
+    }
+}
